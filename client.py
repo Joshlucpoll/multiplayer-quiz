2e43b3968638953d8de1efbeef93af5a7a08c646
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-06-11 19:01:57 +0100

    Finished

--- client.py
+++ client.py
@@ -1,24 +1,28 @@
 import socket
 import pickle
-# Import the functions from the networking tools module
 from fl_networking_tools import get_binary, send_binary
+import time
 
 '''
 Responses
 LIST YOUR RESPONSE CODES HERE
-0 - Initalision
 1 - Game full
 2 - Game not full
 3 - Waiting for other players
-4 - Quiz in progress
+4 - Quiz starting
 5 - Question
 6 - Correct answer
-7 - Incorrect answer
-8 - Quiz stopped
-9 - Results data
+7 - Incorrect attempt
+8 - Incorrect answer
+9 - Quiz finished
+10 - Results data
 '''
 
-# A flag used to control the quiz loop.
+def connectToServer(IP):
+    quiz_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    quiz_server.connect((IP, 2065))
+    return quiz_server
+
 playing = True
 
 IP = input("What is the IP address of the server:\n")
@@ -29,19 +33,60 @@ teamName = input("What is your team name?\n")
 print("\nWelcome " + teamName)
 print("Connecting to Server\n")
 
-quiz_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+quiz_server = connectToServer(IP)
 
-quiz_server.connect((IP, 2065))
+send_binary(quiz_server, ["JOIN", teamName])
 
-print("Connected\n")
-# Sending a command to the server.
-send_binary(quiz_server, ["QUES", ""])
 
 while playing:
-    # The get_binary function returns a list of messages - loop over them
     for response in get_binary(quiz_server):
-        # response is the command/response tuple - response[0] is the code
-        if response[0] == 1: # The question response
+        
+        if response[0] == 1: # Game full
+            disconnectFromServer(quiz_server)
+            print("The lobby is full, please try again later :(")
+            playing = False
+
+        if response[0] == 2: # Game not full
+            print("Connected\n")
+
+        if response[0] == 3: # Waiting for other players
+            print("Waiting for other players. Players connected:\n" + response[1])
+        
+        if response[0] == 4: # Game startting
+            send_binary(quiz_server, ["QUES", ""])
+
+        if response[0] == 5: # The question response
             # Display it to the user.
-            print(response[1])
-            answer = input("")
+            print("\n" + response[1])
+            answer = input()
+            send_binary(quiz_server, ["ANS", answer])
+        
+        if response[0] == 6: # correct ans
+            print("\nYou go that question right!")
+            send_binary(quiz_server, ["QUES", ""])
+
+        if response[0] == 7: # incorrect attempt
+            print("\nSorry you got that question wrong...")
+            print("Try again")
+            send_binary(quiz_server, ["QUES", ""])
+
+        if response[0] == 8: # incorrect ans
+            print("\nSorry you got that question wrong...")
+            print("The correct answer was ", response[1])
+            send_binary(quiz_server, ["QUES", ""])
+        
+        if response[0] == 9: # Quiz finished
+            print("\nYou have completed all the questions in " + str(response[1]) + " seconds.")
+            print("Waiting for other players")
+
+        if response[0] == 10: # results data
+            print("\nThe winner is:")
+            time.sleep(3)
+            scores = response[1]
+            print(str(scores[0][0]) + " with " + str(scores[0][1]) + " points!")
+
+            time.sleep(1)
+            print("\nLeaderBoard:")
+            for i in scores:
+                print(i[0], i[1])
+            
--- server.py
+++ server.py
@@ -2,32 +2,110 @@
 import socketserver
 from collections import namedtuple
 from fl_networking_tools import get_binary, send_binary
-
+from threading import Event
+import time
 '''
 Commands:
 PLACE YOUR COMMANDS HERE
+JOIN - request to join
 QUES - question command
+ANS - answer command
 '''
+global NUMBER_OF_PLAYERS, players, scores, questions, ready_to_start, everyone_finished
 
-# Named tuples are extensions of the tuple structure, with contents you can refer to by name. In this case, the question will be held in a variable named q and the answer in answer.
-# This is just the set up of the question - it will be sent to the client using the send_binary function when a request is made.
+NUMBER_OF_PLAYERS = 2
+players = []
+scores = {}
+
 Question = namedtuple('Question', ['q', 'answer'])
+questions = [
+    Question("What is the name of the bounty hunter who captured Han Solo in The Empire Strikes Back?", "Boba Fett"),
+    Question("What colour was Qui-Gon Jinn’s lightsaber?", "Green"),
+    Question("In The Empire Strikes Back, Lando Calrissian is the Baron Administrator of Cloud City on which planet?", "Bespin"),
+    Question("Jar Jar Binks belongs to what species?", "Gungan"),
+    Question("In what year was the first installment of the Star Wars: Battlefront video game series released?", "2004")
+]
 
-q1 = Question("Expand the acronym ALU:", "Arithmetic Logic Unit")
 
-# The socketserver module uses 'Handlers' to interact with connections. When a client connects a version of this class is made to handle it.
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
 class QuizGame(socketserver.BaseRequestHandler):
-    # The handle method is what actually handles the connection
+    def setup(self):
+        self.startTime = 0
+        self.endTime = 0
+        self.currentQuestion = 0
+        self.attempts = 0
+        self.score = 0
+
     def handle(self):
-        #Retrieve Command
+        global players, NUMBER_OF_PLAYERS, scores, questions, ready_to_start, everyone_finished
+        
         for request in get_binary(self.request):
+            
+            if request[0] == "JOIN":
+                
+                if len(players) >= NUMBER_OF_PLAYERS:
+                    send_binary(self.request, [1, ""]) # game full
+                else:
+                    self.teamName = request[1]
+                    players.append(self.teamName)
+                    send_binary(self.request, [2, ""]) # game not full
+                    
+                    if len(players) == NUMBER_OF_PLAYERS:
+                        # Starts Game
+                        ready_to_start.set()
+                    else:
+                        amountOfPlayers = str(len(players)) + "/" + str(NUMBER_OF_PLAYERS)
+                        send_binary(self.request, [3, amountOfPlayers]) # waiting for other players
+                        # Waits until all player have connected
+                        ready_to_start.wait()
+                    
+                    send_binary(self.request, [4, ""]) # Game started
+                    self.startTime = time.time()
+
+
             if request[0] == "QUES":
-                #Send question
-                send_binary(self.request, (1, q1.q))
-        #Your server code goes here
+                if self.currentQuestion >= len(questions):
+                    # Calculate timetaken
+                    self.endTime = time.time()
+                    timeTaken = self.endTime - self.startTime
+                    send_binary(self.request, [9, round(timeTaken, 2)]) # Send Game end
+
+                    scores[self.teamName] = self.score
+                    if len(scores) == NUMBER_OF_PLAYERS:
+                        everyone_finished.set()
+                    else:
+                        everyone_finished.wait()
+                    
+                    sortedScores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+                    send_binary(self.request, [10, sortedScores])
+                        
+
+                else:
+                    send_binary(self.request, [5, questions[self.currentQuestion].q])
 
 
-# Open the quiz server and bind it to a port - creating a socket
-# This works similarly to the sockets you used before, but you have to give it both an address pair (IP and port) and a handler for the server.
-quiz_server = socketserver.TCPServer(('127.0.0.1', 2065), QuizGame)
+            if request[0] == "ANS":
+                
+                if request[1].lower() == questions[self.currentQuestion].answer.lower(): 
+                    send_binary(self.request, [6, ""]) # Correct ans
+                    self.score += 6 / (self.attempts + 1)
+                    self.currentQuestion += 1
+                    self.attempts = 0
+
+                elif self.attempts < 2:
+                    send_binary(self.request, [7, ""]) # Incorrect attempt
+                    self.attempts += 1
+                    
+                else:
+                    send_binary(self.request, [8, questions[self.currentQuestion].answer]) # Incorrect ans
+                    self.currentQuestion += 1
+                    self.attempts = 0
+                    
+
+
+ready_to_start = Event()
+everyone_finished = Event()
+quiz_server = ThreadedTCPServer(('127.0.0.1', 2065), QuizGame)
 quiz_server.serve_forever()
